33d658cb974badc873c6e55f47d960cd3b61a6c3
Karimullin Arthur <KarimullinArthur@disroot.org>
2024-09-26 12:40:57 +0300

    Added parser for logined-in user

--- bonchapi/parser.py
+++ bonchapi/parser.py
@@ -15,48 +15,52 @@ async def get_lesson_id(html_inp):
     return ids
 
 
-def get_my_lessons(html_inp):
-    soup = BeautifulSoup(html_inp, 'html.parser')
-    table = soup.find('table', class_='simple-little-table')
-    
+async def get_my_lessons(html_inp) -> list[dict]:
+    soup = BeautifulSoup(html_inp, "html.parser")
+    table = soup.find("table", class_="simple-little-table")
+
     schedule_data = []
-    rows = table.find_all('tr')
-    
-    current_day = None
+    rows = table.find_all("tr")
+
     for row in rows:
-        cols = row.find_all('td')
+        cols = row.find_all("td")
         if len(cols) == 1:
             current_day = cols[0].text.strip()
-#             temp = re.compile("([a-zA-Z]+)([0-9]+)")
-#             current_day = temp.match(current_day).groups()
         elif len(cols) > 1:
-            time = cols[0].text.strip()
+            time = cols[0].text.strip().split()[-1]
             number = cols[0].text.strip().split()[0]
-            subject = cols[1].find('b').text.strip()
-            lesson_type = cols[1].find('small').text.strip()
+            subject = cols[1].find("b").text.strip()
+            lesson_type = cols[1].find("small").text.strip()
             location = cols[3].text.strip()
             teacher = cols[4].text.strip()
-    
-            schedule_data.append({
-#                 'date': current_day,
-                'day': current_day,
-                'time': time,
-                'number': number,
-                'subject': subject,
-                'lesson_type': lesson_type,
-                'location': location,
-                'teacher': teacher
-            })
 
-    return schedule_data 
-    
-    
-    
+            # Динамит вне времени...
+            if time[0] == "(":
+                time = cols[0].text.strip().split()[-1][1:-1]
+            
+            # Нажал кнопку начать занятие..
+            if "началось" in lesson_type.split():
+                lesson_type = " ".join(
+                    lesson_type.split()[: lesson_type.split().index("началось") - 1]
+                )
 
-from pprint import pprint
-with open('../index.html') as file:
-    for lesson in get_my_lessons(file):
-        pprint(lesson)
-        print()
-#     for lesson in get_my_lessons(file):
-#         print(lesson)
+            for char in current_day:
+                if char in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0"):
+                    date = current_day[current_day.index(char) :]
+                    current_day = current_day[: current_day.index(char)]
+                    break
+
+            schedule_data.append(
+                {
+                    "date": date,
+                    "day": current_day,
+                    "time": time,
+                    "number": number,
+                    "subject": subject,
+                    "lesson_type": lesson_type,
+                    "location": location,
+                    "teacher": teacher,
+                }
+            )
+
+    return schedule_data
